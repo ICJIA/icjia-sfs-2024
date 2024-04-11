edfa7503ee8a95ef92448eff01d80cf3a531f739
cschweda <cschweda@gmail.com>
2024-04-11 08:44:18 -0500

    fix: adjust vuetify.js plugins

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,12 +1,17 @@
 // import this after install `@mdi/font` package
 import "@mdi/font/css/materialdesignicons.css";
 
+import * as labsComponents from "vuetify/labs/components";
+
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    // ... your configuration
+    icons: {
+      defaultSet: "mdi", // This is already the default value - only for display purposes
+    },
+    ...labsComponents,
   });
   app.vueApp.use(vuetify);
 });
