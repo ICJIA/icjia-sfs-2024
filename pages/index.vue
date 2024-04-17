48516eef855db9e2725e090b4bc05b90115f80d9
cschweda <cschweda@gmail.com>
2024-04-17 13:27:23 -0500

    fix: render splash on client

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
-    <v-img
-      class="bg-grey-lighten-2"
-      src="https://image.icjia.cloud/p7ic5WF0AGoqV0k-qEtINne4Mpk=/1600x600/https://r3.icjia-api.cloud/uploads/getty_images_Q_Ygr_G6nkm4g_unsplash_c29c2710e6.jpg"
-      lazy-src="https://image.icjia.cloud/p7ic5WF0AGoqV0k-qEtINne4Mpk=/1600x600/https://r3.icjia-api.cloud/uploads/getty_images_Q_Ygr_G6nkm4g_unsplash_c29c2710e6.jpg"
-      width="100%"
-      height="800"
-      cover
-      style="margin-top: -65px; filter: grayscale(20%)"
-      gradient="to bottom, rgba(0,0,0,.4), rgba(0,0,0,0)"
-    ></v-img>
+    <v-no-ssr>
+      <v-img
+        class="bg-grey-lighten-2"
+        src="https://image.icjia.cloud/p7ic5WF0AGoqV0k-qEtINne4Mpk=/1600x600/https://r3.icjia-api.cloud/uploads/getty_images_Q_Ygr_G6nkm4g_unsplash_c29c2710e6.jpg"
+        lazy-src="https://image.icjia.cloud/p7ic5WF0AGoqV0k-qEtINne4Mpk=/1600x600/https://r3.icjia-api.cloud/uploads/getty_images_Q_Ygr_G6nkm4g_unsplash_c29c2710e6.jpg"
+        width="100%"
+        height="800"
+        cover
+        style="margin-top: -65px; filter: grayscale(20%)"
+        gradient="to bottom, rgba(0,0,0,.4), rgba(0,0,0,0)"
+      ></v-img>
+    </v-no-ssr>
   </div>
 </template>
 
