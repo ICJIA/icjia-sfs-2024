7a9bb7f1828def6c3c9d454fd1e66ecf6f18236f
cschweda <cschweda@gmail.com>
2024-07-16 21:39:08 -0500

    fix: research query key

--- pages/resources/index.vue
+++ pages/resources/index.vue
@@ -1,9 +1,48 @@
 <template>
-  <div class="mt-16">
-    <h1>Resources here</h1>
+  <div class="pb-16">
+    <v-container
+      ><v-row
+        ><v-col class="text-center mb-6">
+          <h1
+            style="font-size: 40px; margin-top: 80px; text-transform: uppercase"
+          >
+            {{ data?.title }}
+          </h1></v-col
+        >
+      </v-row></v-container
+    >
+
+    <v-container
+      ><v-row
+        ><v-col cols="12" class="mt-0">
+          <ContentDoc
+            :key="data?.title"
+            :value="data"
+            class="markdown-body"
+            style="font-family: 'Lato', sans-serif"
+          >
+            <template #empty>Document not found</template>
+            <template #not-found>Document not found</template>
+          </ContentDoc>
+        </v-col></v-row
+      ></v-container
+    >
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { getThumborUrl } from "@/src/utils/thumbor.js";
+
+let isMounted = ref(false);
+
+const { data } = await useAsyncData(`content-resources`, async () => {
+  const post = await queryContent().where({ _path: "/resources" }).findOne();
+  return post;
+});
+
+onMounted(async () => {
+  isMounted.value = true;
+});
+</script>
 
 <style lang="scss" scoped></style>
